b20a56dc6c31d33f6fbb2ea5b14256c63ab23f7b
root <root@hccweb-v1.dfci.harvard.edu>
2013-07-30 12:38:32 -0400

    Manually added meta keywords and description to template.php file because there are no nodes on the front page and there is no easy way to add meta info otherwise.

--- sites/all/themes/jackson/template.php
+++ sites/all/themes/jackson/template.php
@@ -6,6 +6,25 @@
 function jackson_preprocess_page (&$vars) {
   $vars['preface'] = count(array_filter(array($vars['page']['preface_one'], $vars['page']['preface_two'], $vars['page']['preface_three'])));
   $vars['bottom'] = count(array_filter(array($vars['page']['bottom_one'], $vars['page']['bottom_two'], $vars['page']['bottom_three'], $vars['page']['bottom_four'])));
-  // Display user account links.
+
+	// Add meta description and keywords manually because the homepage has
+	// no actual content nodes
+	if(drupal_is_front_page()){
+		$meta_descr = array(
+			'#tag' => 'meta',
+			'#attributes' => array(
+				'name' => 'description',
+				'content' => 'Dana-Farber Cancer Institute and Harvard Medical Schools Zhang Lab studies the molecular pathogenesis of diffused large B cell lymphoma and EBV-associated B cell lymphoma to identify the surveillance mechanism of the immune system on tumors and develop strategies to harness immune cells for tumor therapy.'),
+		);
+		$meta_keys = array(
+			'#tag' => 'meta',
+			'#attributes' => array(
+				'name' => 'keywords',
+				'content' => 'zhang lab, zhang laboratory, baochun zhang, dfci zhang lab, baochun zhang, harvard research lab, harvard molecular pathogenesis, dfci tumor therapy research, dfci immune system and tumor cells, harvard tumor and immune cell research'),
+		);
+		drupal_add_html_head($meta_descr, 'meta_description');
+		drupal_add_html_head($meta_keys, 'meta_keywords');
+	}
+
 }
 
